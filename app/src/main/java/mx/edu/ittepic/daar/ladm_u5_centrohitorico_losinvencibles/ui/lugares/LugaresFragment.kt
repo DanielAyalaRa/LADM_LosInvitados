b3c3a7c802b19173803de2ca93702de6d96e7bde
Carlos Uriel Fregoso Espericueta <caurfregosoes@ittepic.edu.mx>
2022-05-19 23:27:41 -0600

    Adding the insert places to firestore, missing the upload to cloud the image taken

--- app/src/main/java/mx/edu/ittepic/daar/ladm_u5_centrohitorico_losinvencibles/ui/lugares/LugaresFragment.kt
+++ app/src/main/java/mx/edu/ittepic/daar/ladm_u5_centrohitorico_losinvencibles/ui/lugares/LugaresFragment.kt
@@ -10,8 +10,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 import mx.edu.ittepic.daar.ladm_u5_centrohitorico_losinvencibles.clases.Imagen
 import mx.edu.ittepic.daar.ladm_u5_centrohitorico_losinvencibles.databinding.FragmentLugaresBinding
 
@@ -20,6 +23,7 @@ open class LugaresFragment : Fragment() {
 
     private var _binding: FragmentLugaresBinding? = null
     private val binding get() = _binding!!
+    private val baseRemota = FirebaseFirestore.getInstance().collection("Lugares")
 
     var listaImagenes = ArrayList<Imagen>()
     var contador = 0
@@ -42,6 +46,26 @@ open class LugaresFragment : Fragment() {
             }
             dispararFoto()
             contador++
+        }
+
+        binding.btnInsertar.setOnClickListener {
+            val datos = hashMapOf(
+                "descripcion" to binding.txtDescripcion.text.toString(),
+                "latitud" to binding.txtLatitud.text.toString().toDouble(),
+                "longitud" to binding.txtLongitud.text.toString().toDouble(),
+                "lugar" to binding.txtLugar.text.toString()
+            )
+            baseRemota.add(datos)
+                .addOnSuccessListener {
+                    mensaje("Se guardo el lugar")
+                    binding.txtDescripcion.text?.clear()
+                    binding.txtLugar.text?.clear()
+                    binding.txtLatitud.text?.clear()
+                    binding.txtLongitud.text?.clear()
+                }
+                .addOnFailureListener {
+                    alerta("Error... \n${it.message}")
+                }
         }
 
         return root
@@ -75,5 +99,9 @@ open class LugaresFragment : Fragment() {
             .setTitle("ATENCIÓN")
             .setMessage(cadena)
             .show()
+    }
+
+    fun mensaje(mensaje: String) {
+        Toast.makeText(requireContext(), mensaje, Toast.LENGTH_SHORT).show()
     }
 }
